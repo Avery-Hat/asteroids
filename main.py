ef83e18455e391719f3f2fce2839ed8d06f50522
Avery-Hat <biskatarius@gmail.com>
2024-12-04 10:49:31 -0800

    added loop for game screen. 720p. filled black

--- main.py
+++ main.py
@@ -2,12 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import * #importing all, per course instructions.
 
 def main():
+    pygame.init() #initialized
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #720p
+    running = True
+    black = (0, 0, 0)
+    screen.fill(black) #making background black
 
+    while running: #inf loop to keep game running
+        for quit in pygame.event.get(): 
+            if quit.type == pygame.QUIT:
+                running = False #makes game quit
+        pygame.display.flip() #always at the bottom
+    
+    pygame.quit()
 if __name__ == "__main__":
     main()
